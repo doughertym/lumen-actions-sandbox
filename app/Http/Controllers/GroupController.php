448d0c5615cc9225e0b3c7fdf10308a8d1259cc6
Mike Dougherty <miked@threedev.com>
2020-05-31 22:04:10 -0700

    Get groups from database

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -4,6 +4,8 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\DB;
+
 class GroupController extends Controller
 {
     /**
@@ -17,16 +19,24 @@ class GroupController extends Controller
     }
 
     public function index($church_id) {
-        return response()->json([
-            ['church_id' => $church_id]
-        ]);
+        $groups = DB::table('groups')
+            ->where('church_id', '=', $church_id)
+            ->select([
+                'id', 'church_id', 'campus_id', 'name', 'status',
+                'active', 'address', 'city', 'state', 'zipcode'])
+            ->get();
+        return response()->json($groups);
     }
 
     public function show($church_id, $group_id) {
-        return response()->json([
-            'church_id' => $church_id,
-            'group_id' => $group_id
-        ]);
+        $group = DB::table('groups')
+            ->where('church_id', '=', $church_id)
+            ->where('id', '=', $group_id)
+            ->select([
+                'id', 'church_id', 'campus_id', 'name', 'status',
+                'active', 'address', 'city', 'state', 'zipcode'])
+            ->get();
+        return response()->json($group);
     }
 
 }
