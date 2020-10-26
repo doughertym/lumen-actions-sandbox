85bdd6d2626fe8ece1755a96e1a47ef6a848e87c
Mike Dougherty <miked@threedev.com>
2020-10-25 21:46:16 -0700

    Use the new version file

--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use \Illuminate\Support\Env;
+use Illuminate\Support\Str;
 
 class StatusController extends Controller
 {
-
+    private $logger;
     /**
      * StatusController constructor.
      */
@@ -15,18 +15,40 @@ class StatusController extends Controller
     }
 
     public function index() {
-        $dotenv = \Dotenv\Dotenv::create('/usr/local/gv-ccb', '.version');
-        $version = $dotenv->load();
-
-        return response()->json(
-            [
-                'app' => "{$version['MAJOR']}.{$version['MINOR']}.{$version['PATCH']}-{$version['SHORT_HASH']}",
-                'mysql' => '5.7',
-                'lumen' => '6.0',
-                'version' => $version,
-                'env' => getenv(),
-                'Env' => Env::getVariables()
-            ]
-        );
+        $lumen_version = "v6.0";
+        try {
+            $versions_json = json_decode(
+                file_get_contents(storage_path('versions.json'))
+            );
+            if (filled($versions_json->{'groupvitals-ccb'})) {
+                $ccb_version = $versions_json->{'groupvitals-ccb'};
+                $version = filled($ccb_version) ?
+                    "{$ccb_version->major}.{$ccb_version->minor}.{$ccb_version->patch}" : "";
+                if (filled($ccb_version->branch) && !Str::endsWith($ccb_version->branch, 'production')) {
+                    $version = "${version}-" . Str::slug($ccb_version->branch, '_');
+                }
+                if (filled($ccb_version->hash)) {
+                    $version = "${version}-{$ccb_version->hash}";
+                }
+                if(filled($ccb_version->date)) {
+                    $version .= " on {$ccb_version->date}";
+                }
+            }
+            if(filled($versions_json->lumen)) {
+                $lumen_version = $versions_json->lumen;
+            }
+        } catch (\Exception $exception) {
+            $this->logger->error("Could not load version.json: {$exception->getMessage()}");
+            $version = "unknown";
+        }
+        $status = [
+            env('APP_NAME') => $version,
+            'lumen' => $lumen_version,
+            'mysql' => '5.7.25-google-log', // TODO read from DB::connection()...
+        ];
+        if (env('APP_DEBUG', false)) {
+            $status['Environment'] = $_ENV;
+        }
+        return response()->json($status);
     }
 }
